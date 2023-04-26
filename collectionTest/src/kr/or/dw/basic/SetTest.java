75eb2b360e3c7a1e6ecc2e298f9415179b1d9082
choi-DongJu <ehdehdwn131@gmail.com>
2023-04-26 17:28:31 +0900

    230426

--- collectionTest/src/kr/or/dw/basic/SetTest.java
+++ collectionTest/src/kr/or/dw/basic/SetTest.java
@@ -1,6 +1,10 @@
 package kr.or.dw.basic;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 public class SetTest {
 	/*
@@ -43,6 +47,79 @@ public class SetTest {
 		// 전체삭제 : clear()
 		System.out.println("Set의 데이터 : " + hs1);
 		System.out.println("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
+		
+		
+		HashSet<Integer> intSet = new HashSet<>();
+		intSet.add(10);
+		intSet.add(7);
+		intSet.add(-9);
+		intSet.add(4);
+		intSet.add(5);
+		System.out.println(intSet);
+		//	Set의 모든 데이터의 합계 구하기
+		/*
+		 * Set의 데이터는 순서(index)가 없기 때문에 list 처럼 index로 데이터를 하나씩
+		 * 불러올 수 없다.
+		 * 그래서 데이터를 하나씩 얻기 위해서는 Iterator형 객체로 변환해서 사용해야한다.
+		 * Set형의 데이터를 Iterator형 객체로 변환해주는 메서드는 ==> Iterator()
+		 */
+		
+		Iterator<Integer> it = intSet.iterator();	// Set데이터를 Iterator로 변환하기
+		
+		// Iterator 의 hasNext() 메서드 ==> Iterator 의 데이터를 가리키는 포인터의 다음번째의
+		// 위치에 데이터가 있으면 true, 데이터가 없으면 false를 반환한다.
+		
+		int sum = 0;
+		while(it.hasNext()) {
+			//Iterator의 next()메서드 ==> 포인터를 다음번쨰 위체로 이동한 후 그 자리의 데이터를 반환한다.
+			sum += it.next();
+		}
+		System.out.println("총 합계 : " + sum);
+		
+		// 우리반 학생들 중 번호를 이용하여 추첨하는 프로그램을 작성해보자.
+		// 번호는 1번부터 25번까지 있고, 추첨할 인원은 3명이다
+		// 당첨자를 출력해봅시다.
+		
+		int[] numArr = new int[3];
+		for(int i = 0; i < numArr.length; i++) {
+			int num = 0;
+			while(true) {
+				num = (int)(Math.random() * 25) + 1;
+				if(num != 14 && num != 18 && num != 20 && num != 23) {
+					break;
+				}else {
+					num = (int)(Math.random() * 25) + 1;
+				}
+			}
+			numArr[i] = num;
+		}
+		System.out.println(Arrays.toString(numArr));
+		
+		HashSet<Integer> numSet = new HashSet<>();
+		TreeSet<Integer> treeNumSet = new TreeSet<>();
+		while(numSet.size() < 3) {
+			int num = (int)(Math.random() * 25) + 1;
+			numSet.add(num);
+			treeNumSet.add(num);
+			
+		}
+		System.out.println("numSet의 당첨자 번호 : " +  numSet);
+		System.out.println("treeNumSet의 당첨자 번호 : " +  treeNumSet);
+		
+		// Set 유형의 자료를 List형으로 변환하기
+		ArrayList<Integer> testList = new ArrayList<>(numSet);
+		
+		System.out.println("List 데이터 출력 : " + testList);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
